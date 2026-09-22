769a7429fc73cf1cb39610bd1b76eaf7e69150d3
pascualjm6303 <moon_shay76@yahoo.com>
2026-09-22 13:31:41 -0400

    fix bugs

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,20 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch } from "react-redux";
-import { addItem } from ".CartSlice"; 
+import { addItem } from "./CartSlice"; 
 import { useSelector } from "react-redux";
 
 function ProductList({ onHomeClick }) {
-    const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
+    const [showCart, setShowCart] = useState(false); 
     const dispatch = useDispatch();
-
-    const cartItems = useSelector(state => state.cart.items);
-
+    const CartItems = useSelector(state => state.cart.items);
     const calculateTotalQuantity = () => {
-    return cartItems.reduce((total, item) => total + (item.quantity || 1), 0);
+    return CartItems.reduce((total, item) => total + (item.quantity || 1), 0);
     };
 
     const plantsArray = [
@@ -275,10 +271,7 @@ function ProductList({ onHomeClick }) {
         }));
     };
 
-    const calculateTotalQuantity = () => {
-        return CartItems ? CartItems.reduce((total, item) =>
-           total + item.quantity, 0) : 0;
-     };
+    
 
     
     
